0486e034bb3cf48c40a29c3a3f59e7452a0e92fc
zakariabahlaoui <zakariabahlaoui93@gmail.com>
2023-10-10 19:30:33 +0100

    main function simple_shell

---
+++ simple_shell.c
@@ -0,0 +1,27 @@
+#include "shell.h"
+
+int main(int ac, char **argv)
+{
+    char *line = NULL;
+    char **cmd = NULL;
+
+    while (1)
+    {
+        write(STDOUT_FILENO, "$ ", 2);
+
+        line = read_line();
+
+        if (line == NULL)
+        {
+            if (isatty(STDIN_FILENO))
+                write(STDOUT_FILENO, "\n", 1);
+            return (0);
+        }
+
+        cmd = tokenizer(line);
+        if (cmd == NULL)
+            continue;
+
+        execute_cmd(cmd);
+    }
+}
