58d837e11cf721456c7c251779459c30d50b1e4b
zakariabahlaoui <zakariabahlaoui93@gmail.com>
2023-10-11 09:17:58 +0100

    betty

--- f_execute.c
+++ f_execute.c
@@ -1,24 +1,34 @@
 #include "shell.h"
 
+/**
+ * execute_cmd - take array 2D have a string token to
+ * command and execute it using exeve
+ * @cmd: the 2D array
+ * @argv: argv
+ * @env: environment variable
+ *
+ * Return: is void
+ */
+
 void execute_cmd(char **cmd, char **argv, char **env)
 {
-    pid_t id;
-    int status;
+	pid_t id;
+	int status;
 
-    id = fork();
+	id = fork();
 
-    if (id == 0)
-    {
-        if (execve(cmd[0], cmd, env) == -1)
-        {
-            perror(argv[0]);
-            freearray(cmd);
-            exit(0);
-        }
-    }
-    else
-    {
-        waitpid(id, &status, 0);
-        freearray(cmd);
-    }
+	if (id == 0)
+	{
+		if (execve(cmd[0], cmd, env) == -1)
+		{
+			perror(argv[0]);
+			freearray(cmd);
+			exit(0);
+		}
+	}
+	else
+	{
+		waitpid(id, &status, 0);
+		freearray(cmd);
+	}
 }
