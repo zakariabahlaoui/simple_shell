e9e216db1f7fc1d23028fd4fe0a668d735c68059
zakariabahlaoui <zakariabahlaoui93@gmail.com>
2023-10-12 11:38:33 +0100

    betty for resd_line

--- readline.c
+++ readline.c
@@ -1,18 +1,24 @@
 #include "shell.h"
 
+/**
+ * read_line - get the line after prompet and assignet in string
+ *
+ * Return: the line
+ */
+
 char *read_line(void)
 {
-    char *line = NULL;
-    size_t length = 0;
-    size_t i;
+	char *line = NULL;
+	size_t length = 0;
+	size_t i;
 
-    i = getline(&line, &length, stdin);
+	i = getline(&line, &length, stdin);
 
-    if (i == -1)
-    {
-        free(line);
-        return (NULL);
-    }
+	if (i == -1)
+	{
+		free(line);
+		return (NULL);
+	}
 
-    return (line);
+	return (line);
 }
