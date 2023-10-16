574fe430cc955b0c9f9dc0c40e3c2bab4d5ff8c0
zakariabahlaoui <zakariabahlaoui93@gmail.com>
2023-10-16 14:07:30 +0100

    bett

--- simple_shell.c
+++ simple_shell.c
@@ -1,36 +1,40 @@
 #include "shell.h"
 
+/**
+ * main - main simple shell
+ * @ac: argc
+ * @argv: argv
+ * @env: envirment varible
+ *
+ * Return: return
+ */
+
 int main(int ac, char **argv, char **env)
 {
-    char *line = NULL;
-    char **cmd = NULL;
-    int index = 0;
-    int stat = 0;
+	char *line = NULL;
+	char **cmd = NULL;
+	int index = 0;
+	int stat = 0;
 
-    while (1)
-    {
-        if (isatty(STDIN_FILENO))
-            write(STDOUT_FILENO, "$ ", 2);
+	while (1)
+	{
+		if (isatty(STDIN_FILENO))
+			write(STDOUT_FILENO, "$ ", 2);
 
-        line = read_line();
+		line = read_line();
 
-        if (line == NULL)
-        {
-            if (isatty(STDIN_FILENO))
-                write(STDOUT_FILENO, "\n", 1);
-            return (stat);
-        }
-        index++;
+		if (line == NULL)
+		{
+			if (isatty(STDIN_FILENO))
+				write(STDOUT_FILENO, "\n", 1);
+			return (stat);
+		}
+		index++;
 
-        cmd = _tokenizer(line);
-        if (cmd == NULL)
-            continue;
-        // for (i = 0; cmd[i]; i++)
-        // {
-        //     printf("%s\n", cmd[i]);
-        // }
-        // freearray(cmd);
+		cmd = _tokenizer(line);
+		if (cmd == NULL)
+			continue;
 
-        stat = execute_cmd(cmd, argv, env, index);
-    }
+		stat = execute_cmd(cmd, argv, env, index);
+	}
 }
